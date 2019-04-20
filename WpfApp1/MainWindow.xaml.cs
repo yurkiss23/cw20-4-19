835e14435250236a804912af11387818cd0e4432
User <User@A15COMP6>
2019-04-20 10:42:39 +0300

    add DB

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -22,43 +26,89 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SqlConnection _connect;
         private ObservableCollection<User> users = new ObservableCollection<User>();
+        private string conStr = ConfigurationManager.AppSettings["conStr"];
         public MainWindow()
         {
             InitializeComponent();
-            users.Add(new User() { Name = "qwe rty" });
-            users.Add(new User() { Name = "asd fgh" });
+            _connect = new SqlConnection(conStr);
+            users.Add(new User() { FName = "qwe", LName = "rty" });
+            users.Add(new User() { FName = "asd", LName = "fgh" });
 
-            lbUsers.ItemsSource = users;
+            DataTable dt = new DataTable();
+            DataColumn id = new DataColumn("id", typeof(int));
+            DataColumn firstname = new DataColumn("firstname", typeof(string));
+            DataColumn lastname = new DataColumn("lastname", typeof(string));
+            dt.Columns.Add(id);
+            dt.Columns.Add(firstname);
+            dt.Columns.Add(lastname);
+
+
+            //lbUsers.ItemsSource = users;
+            using (TransactionScope scope = new TransactionScope())
+            {
+                _connect.Open();
+                SqlCommand cmd = new SqlCommand("SELECT [Id],[LastName],[FirstName]FROM[yurkissdb].[dbo].[exam_Students]", _connect);
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    DataRow row = dt.NewRow();
+                    row[0] = rdr["Id"];
+                    row[1] = rdr["FirstName"].ToString();
+                    row[2] = rdr["LastName"].ToString();
+                    dt.Rows.Add(row);
+                }
+
+                _connect.Close();
+                scope.Complete();
+            }
+            DG.ItemsSource = dt.DefaultView;
         }
         private void btnAddUser_Click(object sender, RoutedEventArgs e)
         {
-            users.Add(new User() { Name = "New user" });
+            users.Add(new User() { FName = "New user",LName="new user" });
         }
 
         private void btnChangeUser_Click(object sender, RoutedEventArgs e)
         {
-            if (lbUsers.SelectedItem != null)
-                (lbUsers.SelectedItem as User).Name = "Random Name";
+            if (DG.SelectedItem != null)
+            {
+                (DG.SelectedItem as User).FName = "Random Name";
+                (DG.SelectedItem as User).LName = "random name";
+            }
         }
 
         private void btnDeleteUser_Click(object sender, RoutedEventArgs e)
         {
-            if (lbUsers.SelectedItem != null)
-                users.Remove(lbUsers.SelectedItem as User);
+            if (DG.SelectedItem != null)
+                users.Remove(DG.SelectedItem as User);
         }
     }
     public class User : INotifyPropertyChanged
     {
-        private string name;
-        public string Name
+        private string fName;
+        private string lName;
+        public string FName
         {
-            get { return this.name; }
+            get { return this.fName; }
             set
             {
-                if (this.name != value)
+                if (this.fName != value)
                 {
-                    this.name = value;
+                    this.fName = value;
+                    this.NotifyPropertyChanged("Name");
+                }
+            }
+        }
+        public string LName
+        {
+            get { return this.lName; }
+            set
+            {
+                if (this.lName != value)
+                {
+                    this.lName = value;
                     this.NotifyPropertyChanged("Name");
                 }
             }
